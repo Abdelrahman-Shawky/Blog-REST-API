53eb6c261ecdd008d90253c51af3749e36f267e4
Abdelrahman Shawky <abdelrahman.a.shawky@gmail.com>
2023-03-18 22:02:18 +0200

    jwt authentication

--- Back-End/controllers/auth.js
+++ Back-End/controllers/auth.js
@@ -1,6 +1,9 @@
 const User = require('../models/user');
 const {validationResult} = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 
 exports.signup = (req, res, next) => {
@@ -26,6 +29,41 @@ exports.signup = (req, res, next) => {
     })
     .then(result => {
         res.status(201).json({message: 'User Created',  userId: result._id});
+    })
+    .catch(err => {
+        if (!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    })
+};
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({email: email})
+    .then(user => {
+        if(!user) {
+            const error = new Error('A user with this email could not be found');
+            error.statusCode = 401;
+            throw error;
+        }
+        loadedUser = user;
+        return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+        if(!isEqual) {
+            const error = new Error('Wrong Password');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            email: loadedUser.email,
+            userId: loadedUser._id.toString()
+        }, process.env.SECRET_KEY, 
+        {expiresIn: '1hr'});
+        res.status(200).json({token: token, userId: loadedUser._id.toString()});
     })
     .catch(err => {
         if (!err.statusCode){
--- Back-End/routes/feed.js
+++ Back-End/routes/feed.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const feedController = require('../controllers/feed');
 const {body} = require('express-validator/check');
+const isAuth = require('../middleware/is-auth')
 
 const router = express.Router();
 
 
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 router.get('/post/:postId', feedController.getPost);
 
-router.post('/post', [
+router.post('/post', isAuth, [
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ], feedController.createPost);
 
-router.put('/post/:postId', [
+router.put('/post/:postId', isAuth, [
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ], feedController.updatePost);
 
-router.delete('/post/:postId', feedController.deletePost)
+router.delete('/post/:postId', isAuth, feedController.deletePost)
 
 module.exports = router;
